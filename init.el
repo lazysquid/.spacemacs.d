176f8538a07b03511fcb30800d59019a315e1168
fpenguin23 <fpenguin23@gmail.com>
2017-09-29 22:42:49 +0900

    Delete themes-megapack, add dracula theme

--- init.el
+++ init.el
@@ -59,7 +59,7 @@ values."
      (latex :variables
             latex-build-command "xelatex"
             )
-     themes-megapack
+     ;;themes-megapack
      emacs-lisp
      ranger
      (shell :variables
@@ -156,7 +156,8 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
+   dotspacemacs-themes '(dracula
+                         spacemacs-dark
                          kaolin
                          monokai)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
