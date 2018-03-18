91b9b4f2a55c47620c9b77549a6052fbbd95b70b
wonjun <wonjunlee.0729@gmail.com>
2018-03-18 23:34:14 +0900

    Add korean language setup

--- init.el
+++ init.el
@@ -374,6 +374,14 @@ before packages are loaded."
 
   ;; Insert tab instead of indent!
   (setq tab-always-indent nil)
+
+  ;; Korean language setup
+  (set-language-environment "Korean")
+  (set-fontset-font t 'hangul (font-spec :name "NanumGothic"))
+  (global-set-key (kbd "<kana>") 'toggle-input-method)
+  (global-set-key (kbd "<S-spc>") 'toggle-input-method)
+  (prefer-coding-system 'utf-8)
+
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
